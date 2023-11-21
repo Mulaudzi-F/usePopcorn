6abd0928c5d9b7812dd2e0ea9f4adc938411ffe0
Fhatuwani Mulaudzi <mulaudzifhatuwanib20@gmail.com>
2023-11-21 12:03:05 +0200

    created star components

--- vite-project/src/main.jsx
+++ vite-project/src/main.jsx
@@ -1,10 +1,28 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+// import App from './App.jsx'
+// import './index.css'
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+import StarRating from "./StarRating";
+
+function Test() {
+  const [movieRating, setMovieRating] = React.useState(0);
+
+  return (
+    <div>
+      <StarRating color="blue" maxRating={10} onSetRating={setMovieRating} />
+      <p>This movie was rated {movieRating} stars</p>
+    </div>
+  );
+}
+
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    {/* /<App /> */}
+    <StarRating
+      maxRating={5}
+      messages={["Terrible", "Bad", "Okay", "Good", "Amazing"]}
+      defaultRating={3}
+    />
+  </React.StrictMode>
+);
